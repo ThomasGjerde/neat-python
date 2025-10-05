e89069a2078bdbdf69db3de2945079cbb3c57687
Thomas Gjerde <thomasgjerde90@gmail.com>
2025-10-05 17:24:03 +0200

    Use absolute path in test checkpoint prefix

--- tests/config.py
+++ tests/config.py
@@ -1,3 +1,5 @@
-from os import path
-CHECKPOINT_DIR = path.join("tests", "checkpoints")
-DEFAULT_CHECKPOINT_PREFIX = path.join(CHECKPOINT_DIR, "neat-checkpoint-")
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CHECKPOINT_DIR = os.path.join(BASE_DIR, "checkpoints")
+DEFAULT_CHECKPOINT_PREFIX = os.path.join(CHECKPOINT_DIR, "neat-checkpoint-")
